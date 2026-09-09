5f13c9943b8671d6a97948a516a79244bd1a39af
231yz666yz <x00217230143@njit.edu.cn>
2026-09-09 14:48:14 +0800

    Submit

--- tuple_operations.py
+++ tuple_operations.py
@@ -15,29 +15,55 @@ valid_days = (
 
 # Write logic to first accept course code from the user, then course name, credits, and semester. 
 # Store them in a tuple called course.
-
-
-
-
+course_code = input("Enter your course code: ")
+course_name = input("Enter your course name: ")
+course_credits = int(input("Enter your course credits: "))
+course_semester = input("Enter your course semester: ")
+course = (course_code, course_name, course_credits, course_semester)
 
 # Accept course schedule information like the day, start time, end time, and room. Store them in a tuple called schedule.
-
-
-
+day = input("Enter the day: ")
+start_time = input("Enter the start time: ")
+end_time = input("Enter the end time: ")
+room = input("Enter the room: ")
+schedule = (day, start_time, end_time, room)
 
 # Add the schedule tuple to the course tuple to create a nested tuple.
-
-
-
+nested = course + schedule
 
 # Make sure the schedule is complete. Meaning, it doesn't have any empty values.
-
+if '' in schedule:
+    schedule_check = "NO"
+    print("The schedule is not completed")
+else:
+    schedule_check = "YES"
 
 # Check if the day is valid. Meaning, it should be one of the valid days defined above.
-
+if day not in valid_days:
+    day_check = "NO"
+    print("The day is invalid")
+else:
+    day_check = "YES"
 
 # Print the course information to look like the following:
+print(f"""====================================
+COURSE INFORMATION
+====================================
 
+Course: {course_name}
+Credits: {course_credits}
+Semester: {course_semester}
+
+------------- SCHEDULE -------------
+Day: {day}
+Time: {start_time} - {end_time}
+Room: {room}
+
+Schedule Complete: {schedule_check}
+Class Day Valid: {day_check}
+
+==================================== 
+""")
 
 """ 
 ====================================
